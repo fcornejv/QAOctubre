63857feedb484343e91fcc73bca61ab7f5340fa8
fcornejv <frank.pcv@gmail.com>
2025-11-27 01:05:30 -0500

    UnHappy path Busqueda Eventos
    UnHappy path Busqueda Eventos

--- cypress/e2e/Challenge_Busqueda_Eventos.cy.js
+++ cypress/e2e/Challenge_Busqueda_Eventos.cy.js
@@ -68,12 +68,12 @@ describe('Módulo de Búsqueda de Eventos (Requiere Login)', () => {
           .contains('button', '1') // Dentro de esa fila, localiza el botón con el texto '1'
           .should('be.visible')
           .click(); 
-        
+        /*
         // El asiento 1 en la F5 tiene bg-blue-500, podemos verificar su clase o el indicador.
         cy.contains('div.flex-nowrap', 'F5')
           .contains('button', '1')
           .should('have.class', 'bg-blue-500'); // Verifica que el color haya cambiado a 'Seleccionado'
-
+*/
         cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
         
         cy.contains('span', 'Comprar (1)')
@@ -193,25 +193,100 @@ describe('Módulo de Búsqueda de Eventos (Requiere Login)', () => {
         cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
 
 
-cy.log('Presionando el botón "Ver todas las entradas" dentro de la tarjeta específica');
+        cy.log('Presionando el botón "Ver todas las entradas" dentro de la tarjeta específica');
 
-// 1. Localiza el contenedor de la tarjeta del ticket usando su data-cy
-cy.get('[data-cy="ticket-card-10"]') 
-  // 2. Usar .within() para enfocar las búsquedas en este contenedor
-  .within(() => {
-    // 3. Buscar el botón por su texto, sabiendo que está dentro de la tarjeta ticket-card-10
-    cy.contains('button', 'Ver todas las entradas')
-      .should('be.visible')
-      .click(); 
-  });
+        // 1. Localiza el contenedor de la tarjeta del ticket usando su data-cy
+        cy.get('[data-cy="ticket-card-10"]') 
+        // 2. Usar .within() para enfocar las búsquedas en este contenedor
+        .within(() => {
+            // 3. Buscar el botón por su texto, sabiendo que está dentro de la tarjeta ticket-card-10
+            cy.contains('button', 'Ver todas las entradas')
+            .should('be.visible')
+            .click(); 
+        });
 
-cy.wait(2000); // Mantenemos el wait por si la navegación es lenta
+        cy.wait(2000); // Mantenemos el wait por si la navegación es lenta
 
 
         cy.log('Presionando el botón "Ver entrada"');
         cy.contains('button', 'Ver entrada')
           .click();        
         cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+
+    });
+
+     it('UnHappy path - No puedes seleccionar más de 4 asientos por persona', () => {
+        cy.log('Iniciando prueba de flujo completo: Búsqueda -> Panel -> Adquirir -> Selección de fecha y hora.');
+        
+        cy.get('[placeholder="Busca tu próxima función!"]').type('Evento Multiple Free'); 
+        cy.get('[data-cy="btn-ver-evento-10"]')
+        .should('be.visible')
+        .click(); 
+        
+        cy.contains('button', 'Adquirir entrada')
+        .scrollIntoView() // Fuerza a Cypress a desplazar la vista hasta que el elemento sea visible
+        .should('be.visible')
+        .click(); 
+
+        cy.contains('span', 'domingo, 15 de junio de 2025')
+        .should('be.visible')
+        .click(); 
+        
+        cy.contains('button', '15:00 hs', { timeout: 10000 }) 
+        .should('be.visible')
+        .click();
+        cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+        
+        cy.log('Presionando el botón "Continuar con la compra"');
+        cy.contains('button', 'Continuar con la compra')
+          .should('be.visible')
+          .click();        
+        cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+
+        cy.log('Seleccionando la zona: Palco');
+        cy.contains('Palco')
+          .should('be.visible')
+          .click();
+        cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+
+
+
+        cy.log('Seleccionando asiento: F5, 1');
+        
+        // Aserción: Asegurar que el mapa de asientos esté visible
+        cy.get('div.flex-nowrap').should('be.visible'); 
+        
+        cy.contains('div.flex-nowrap', 'F9') // Localiza la fila que contiene F5
+          .contains('button', '1') // Dentro de esa fila, localiza el botón con el texto '1'
+          .should('be.visible')
+          .click(); 
+        
+        // El asiento 1 en la F5 tiene bg-blue-500, podemos verificar su clase o el indicador.
+        cy.contains('div.flex-nowrap', 'F9')
+          .contains('button', '1')
+          .should('have.class', 'bg-blue-500'); // Verifica que el color haya cambiado a 'Seleccionado'
+
+        cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+        
+        cy.contains('div.flex-nowrap', 'F9') // Localiza la fila que contiene F5
+          .contains('button', '2') // Dentro de esa fila, localiza el botón con el texto '1'
+          .should('be.visible')
+          .click(); 
+        cy.contains('div.flex-nowrap', 'F9') // Localiza la fila que contiene F5
+          .contains('button', '3') // Dentro de esa fila, localiza el botón con el texto '1'
+          .should('be.visible')
+          .click(); 
+                  cy.contains('div.flex-nowrap', 'F9') // Localiza la fila que contiene F5
+          .contains('button', '4') // Dentro de esa fila, localiza el botón con el texto '1'
+          .should('be.visible')
+          .click(); 
+
+cy.contains('p', 'No puedes seleccionar más de 4 asientos por persona.')
+  .should('be.visible');
+
+        cy.wait(2000); // Espera 2 segundos después de escribir para que el filtro actúe
+
+
 
     });
 
